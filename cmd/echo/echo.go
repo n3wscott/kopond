5ffe0784993f70bd9032f2121530953835bcc147
Scott Nichols <nicholss@google.com>
2018-07-11 11:06:03 -0700

    add log and service.

--- cmd/echo/echo.go
+++ cmd/echo/echo.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you want: %s\n", r.URL.Path)
+		log.Printf("Request: %s", r.URL.Path)
 	})
 
 	http.ListenAndServe(":8080", nil)
